c3431393bf04c7aedb4692aa31c03ab6ad83b531
“[Stanislav <[stan.rashevsky@gmail.com]>
2019-09-29 17:13:13 -0400

    can show alert msgs, and delete books

--- app.js
+++ app.js
@@ -19,6 +19,29 @@ UI.prototype.addBookToList = function(book) {
   <td><a href="#" class="delete">X</a></td>`;
   list.appendChild(row);
 };
+
+// Show alert
+UI.prototype.showAlert = function(msg, className) {
+  const div = document.createElement("div");
+  div.className = `alert ${className}`;
+  div.appendChild(document.createTextNode(msg));
+  const container = document.querySelector(".container");
+  const form = document.querySelector("#book-form");
+  container.insertBefore(div, form);
+
+  // timeout after 3 sec
+  setTimeout(function() {
+    document.querySelector(".alert").remove();
+  }, 3000);
+};
+
+// Delete book
+UI.prototype.deleteBook = function(target) {
+  if (target.className === "delete") {
+    target.parentElement.parentElement.remove();
+  }
+};
+// Clear fields
 UI.prototype.clearFields = function() {
   (document.getElementById("title").value = ""),
     (author = document.getElementById("author").value = ""),
@@ -26,6 +49,8 @@ UI.prototype.clearFields = function() {
 };
 
 // Event Listeners
+
+// Event Listener for delete
 document.getElementById("book-form").addEventListener("submit", function(e) {
   e.preventDefault();
 
@@ -39,7 +64,28 @@ document.getElementById("book-form").addEventListener("submit", function(e) {
 
   // instantiate UI
   const ui = new UI();
-  // Add book to list
-  ui.addBookToList(book);
-  ui.clearFields();
+
+  // validate input
+  if (title === "" || author === "" || isbn === "") {
+    // Error alert
+    ui.showAlert("Please fill in all fields", "error");
+  } else {
+    // Add book to list
+    ui.addBookToList(book);
+    // show success alert
+    ui.showAlert("Book Added!", "success");
+    // clear fields
+    ui.clearFields();
+  }
+});
+
+// Event Listener for Delete
+document.getElementById("book-list").addEventListener("click", function(e) {
+  e.preventDefault();
+  // instantiate UI
+  const ui = new UI();
+  // delete book
+  ui.deleteBook(e.target);
+  // show msg
+  ui.showAlert("Book Removed!", "success");
 });
